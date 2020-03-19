e5ba49adfcc46adb069d9943be54396a55b26a9f
MackoZBogdanca <mackozbogdanca@yandex.com>
2020-03-19 21:07:22 +0100

    create basic functionality

---
+++ script.js
@@ -0,0 +1,31 @@
+const inputValue = document.getElementById("search");
+const searchButton = document.querySelector(".search-button");
+const nameContainer = document.querySelector(".profile-name");
+const userNameContainer = document.querySelector(".profile-username");
+const reposContainer = document.querySelector(".profile-repos");
+const urlContainer = document.querySelector(".profile-url");
+
+const client_id = "Iv1.95f7d52a57185b0c";
+const client_secret = "11bb4d7fd89247b32cdce162de50d79f7f291eee";
+
+const fetchUsers = async user => {
+  const fetchApi = await fetch(
+    `https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`
+  );
+
+  const data = await fetchApi.json();
+  return { data };
+};
+
+const showData = () => {
+  fetchUsers(inputValue.value).then(res => {
+    nameContainer.innerHTML = `Name: <span class="profile-value">${res.data.name}</span>`;
+    userNameContainer.innerHTML = `Username: <span class="profile-value">${res.data.login}</span>`;
+    reposContainer.innerHTML = `Repos: <span class="profile-value">${res.data.public_repos}</span>`;
+    urlContainer.innerHTML = `URL: <span class="profile-value">${res.data.html_url}</span>`;
+  });
+};
+
+searchButton.addEventListener("click", () => {
+  showData();
+});
